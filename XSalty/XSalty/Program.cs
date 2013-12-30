4f337661bc0ed376810314040f1ae0ac5bf7de19
sgbj <scott.batary@gmail.com>
2013-12-30 10:22:54 -0600

    updated

--- XSalty/XSalty/Program.cs
+++ XSalty/XSalty/Program.cs
@@ -1,18 +1,45 @@
 ﻿namespace XSalty
 {
     using System;
-    using System.IO;
     using System.Xml.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var xml = File.ReadAllText("message.xml");
-            var template = File.ReadAllText("warrant-collection-template.csxml");
-            var doc = XDocument.Parse(xml);
-            var text = doc.XSalty(template);
-            File.WriteAllText("warrant-collection.xml", text);
+            var doc =
+                new XDocument(
+                    new XElement("customers",
+                        new XElement("person",
+                            new XElement("name", "John Doe"),
+                            new XElement("age", 37)),
+                        new XElement("person",
+                            new XElement("name", "Jane Doe"),
+                            new XElement("age", 35))));
+            var template =
+@"<!doctype html>
+<html>
+    <head>
+        <title>Customers</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+            </tr>
+            @foreach (var person in Eval(""customers/person""))
+            {
+            <tr>
+                <td>@Eval(""string(name)"", person)</td>
+                <td>@Eval(""number(age)"", person)</td>
+            </tr>
+            }
+        </table>
+    </body>
+</html>";
+            var output = doc.XSalty(template);
+            Console.WriteLine(output);
         }
     }
 }
--- XSalty/XSalty/XSalty.cs
+++ XSalty/XSalty/XSalty.cs
@@ -21,22 +21,13 @@
 
     public abstract class XSaltyTemplate<T> : TemplateBase<T> where T : XDocument
     {
-        public dynamic Eval(string xpath)
+        public dynamic Eval(string xpath, XNode node = null)
         {
             var namespaceManager = new XmlNamespaceManager(new NameTable());
             foreach (var element in Model.Descendants())
                 foreach (var attribute in element.Attributes().Where(a => a.IsNamespaceDeclaration))
                     namespaceManager.AddNamespace(attribute.Name.LocalName, XNamespace.Get(attribute.Value).NamespaceName);
-            return Model.XPathEvaluate(xpath, namespaceManager);
-        }
-
-        public dynamic Eval(XNode node, string xpath)
-        {
-            var namespaceManager = new XmlNamespaceManager(new NameTable());
-            foreach (var element in Model.Descendants())
-                foreach (var attribute in element.Attributes().Where(a => a.IsNamespaceDeclaration))
-                    namespaceManager.AddNamespace(attribute.Name.LocalName, XNamespace.Get(attribute.Value).NamespaceName);
-            return node.XPathEvaluate(xpath, namespaceManager);
+            return (node ?? Model).XPathEvaluate(xpath, namespaceManager);
         }
     }
 }
